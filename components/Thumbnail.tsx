a09d171cd4212c9f6640ea2c08f3af579563937a
lonelybuddy <henryzheng92@gmail.com>
2022-05-04 04:47:34 -0500

    sucessfully extracted the certification codes for movies and tvs

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -7,6 +7,8 @@ type Props = {
 };
 
 export const Thumbnail = memo(({ movie }: Props) => {
+  //   console.log(movie);
+
   return (
     <div className="relative min-h-[140px] min-w-[240px] cursor-pointer transition duration-200 ease-out md:min-h-[230px] md:min-w-[470px] md:hover:scale-105">
       <Image
@@ -18,9 +20,25 @@ export const Thumbnail = memo(({ movie }: Props) => {
       />
 
       <div className="absolute">
-        <span></span>
-        <h2>{movie.title}</h2>
+        <div>
+          <div>{movie.release_date || movie.first_air_date}</div>
+          <div>{movie.media_type}</div>
+        </div>
+        {/* <h2>{movie.title ? movie.title : movie.name}</h2> */}
+        <h2>{movie.title || movie.name}</h2>
       </div>
     </div>
   );
 });
+
+/*
+// url to get certification
+
+Movie certification
+
+https://api.themoviedb.org/3/movie/453395/release_dates?api_key=f0f104174970501943df661d92b61131
+
+TV certification
+
+https://api.themoviedb.org/3/tv/60059/content_ratings?api_key=f0f104174970501943df661d92b61131&language=en-US
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import requests from '../utils/requests';
 import { Movie, TV } from '../typings';
 import { Search } from '../components/Search';
 import { Trending } from '../components/Trending';
+import { useEffect } from 'react';
+import Movies from './movies';
 
 export type Props = {
   trendingNow: Movie[];
@@ -11,6 +13,8 @@ export type Props = {
 };
 
 const Home = ({ trendingNow, popularMovies, popularTVs }: Props) => {
+  console.log('===>', trendingNow);
+
   return (
     <>
       <Head>
@@ -34,10 +38,50 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchTVs).then((res) => res.json()),
   ]);
 
+  const certifications = await Promise.all(
+    trendingNow.results.map((trending) =>
+      trending.media_type === 'movie'
+        ? fetch(
+            `https://api.themoviedb.org/3/movie/${trending.id}/release_dates?api_key=f0f104174970501943df661d92b61131&language=en-US`
+          ).then((res) => res.json())
+        : fetch(
+            `https://api.themoviedb.org/3/tv/${trending.id}/content_ratings?api_key=f0f104174970501943df661d92b61131&language=en-US`
+          ).then((res) => res.json())
+    )
+  ).then((res) => {
+    let reformed = res.map(({ results }, idx) => {
+      const newObj = {};
+
+      results.forEach((obj: { iso_3166_1: string; rating: string }) => {
+        if (obj.iso_3166_1 === 'US') {
+          if (obj.rating) {
+            newObj.certification = obj.rating;
+          } else {
+            const cert = obj.release_dates.find(
+              (obj1) => obj1.certification !== ''
+            );
+
+            newObj.certification = cert ? cert.certification : null;
+          }
+        }
+      });
+
+      return newObj;
+    });
+
+    return reformed;
+  });
+
+  const trendingWithCerts = trendingNow.results.map((movie, idx) => {
+    movie.certification = certifications[idx].certification;
+
+    return movie;
+  });
+
   return {
     props: {
-      trendingNow: trendingNow.results,
-      popularMovies: fetchMovies.results,
+      trendingNow: trendingWithCerts,
+      popularMovies: certifications,
       popularTVs: fetchTVs.results,
     },
   };
