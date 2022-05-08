52a8dd1f8faf8cf8d47f65ab87aa2cb7e11d10a4
lonelybuddy <henryzheng92@gmail.com>
2022-05-07 22:32:45 -0500

    cleaned up imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import { SidebarMenu } from '../components/SidebarMenu';
 import { Layout } from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
